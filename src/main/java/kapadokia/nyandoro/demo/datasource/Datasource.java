4e2907c30219f9ad4334fbc0793c365180691b53
kapadokia <ktnyandoch@gmail.com>
2022-09-18 15:24:14 +0300

    added docker migration properties, working

---
+++ src/main/java/kapadokia/nyandoro/demo/datasource/Datasource.java
@@ -0,0 +1,20 @@
+package kapadokia.nyandoro.demo.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Datasource{
+
+    //define datasource
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource (){
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
